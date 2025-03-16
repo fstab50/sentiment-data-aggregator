bd9a0d506e0a992decdc61f9b0925b9213ff22ba
Blake Huber <blakeca00@gmail.com>
2025-03-16 17:49:27 -0500

    hooks/post-commit-versionupdate.py: updated to completely custom version for lambda functions

--- hooks/post-commit-versionupdate.py
+++ hooks/post-commit-versionupdate.py
@@ -1,57 +1,131 @@
 #!/usr/bin/env python3
 """
+Summary.
 
-   - Summary:   Post commit hook, updates version throughout project
-   - Location:  .git/hooks
-   - Filename:  commit-msg
+   Post commit hook, updates version throughout project
+
+Location:  .git/hooks
+
+Filename:  commit-msg
 
 """
 import os
 import sys
 import re
 import inspect
+import fileinput
+import subprocess
+from pyaws.utils import stdout_message
+
+# globals
+module = os.path.basename(__file__)
+lib_relpath = 'Code'                # path relative to git project root
+version_module = 'version'
+targets = ['README.md']
+
+
+def git_root():
+    """Locate root directory of git repository."""
+    cmd = 'git rev-parse --show-toplevel 2>/dev/null'
+    return subprocess.getoutput(cmd).strip()
 
 
 def packagename(filename):
-    with open(filename) as p1:
-        for line in p1.readlines():
-            if 'PACKAGE' in line:
-                return line.split(':')[1].strip()
-                break
+    """Retrieve name of build package stored locally."""
+    if os.path.exists(filename):
+        with open(filename) as p1:
+            for line in p1.readlines():
+                if 'PACKAGE' in line:
+                    return line.split(':')[1].strip()
+                    break
+                else:
+                    return None
+    return None
 
 
-PACKAGE = packagename('DESCRIPTION.rst') or None
-pattern = re.compile('^\*\*Version\*\*')
+def deprecated_version(filename, expression):
+    """
+    Summary.
+
+        Extract program version N-1.
+
+    Args:
+        :filename (str): Name of file contents searched for N-1 version num.
+        :expression (str): Regex or string which matches deprecated version
+
+    Returns:
+        exact match, TYPE: str or None
+
+    """
+    pattern = re.compile(expression)
+
+    try:
+        with open(filename) as d1:
+            parsed = set(d1.read().split())
+            for item in parsed:
+                if pattern.match(item):
+                    return item
+    except OSError:
+        stdout_message(
+                message=f'File {filename} not found', prefix='DBUG'
+            )
+    except Exception as e:
+        stdout_message(
+                message=f'Unknown error ({e})', prefix='DBUG'
+            )
+    return None
+
+
+def incremental_version(old, new):
+    """Determine if version label has changed."""
+    return False if old == new else True
+
+
+PACKAGE = packagename('DESCRIPTION.rst')
+CURRENT = deprecated_version('README.md', '[0-9]\.[0-9]\.[0-9]')
 
 if PACKAGE is None:
-    print('Problem executing post-commit-hook (%s). Exit' % __file__)
-    sys.exit(1)
+
+    try:
+
+        # adj path down 1 level
+        sys.path.insert(0, os.path.abspath(git_root() + '/' + lib_relpath))
+
+        from version import __version__
+
+        # normalize path
+        sys.path.pop(0)
+
+    except ImportError as e:
+        stdout_message(
+            message='Problem executing commit-hook (%s). Error: %s' %
+            (__file__, str(e)),
+            prefix='WARN'
+            )
 else:
     sys.path.insert(0, os.path.abspath(PACKAGE))
-    from _version import __version__
+    from version_module import __version__
     sys.path.pop(0)
 
 
 try:
-    with open('README.md') as f1:
-        lines = f1.readlines()
-        for index, line in enumerate(lines):
-            if 'Version:' in line:
-                newline = '  Version: ' + __version__ + '\n'
-                lines[index] = newline
-                break
+    if not list(filter(lambda x: os.path.exists(x), targets)):
+        stdout_message(
+            message=f'One or more commit-hook targets ({targets}) not found',
+            prefix='WARN'
+        )
+        sys.exit(1)
 
-            elif pattern.match(line):
-                newline = '**Version**: ' + __version__ + '\n'
-                lines[index] = newline
-                break
+    elif incremental_version(CURRENT, __version__):
 
-        f1.close()
-        with open('README.md', 'w') as f3:
-            f3.writelines(lines)
+        # update specfile - major version
+        for line in fileinput.input(targets, inplace=True):
+            print(line.replace(CURRENT, __version__), end='')
+        stdout_message(f'Updated {targets} version {CURRENT} with {__version__}', prefix='OK')
+
 except OSError as e:
-    print(
-            '%s: Error while reading or writing post-commit-hook (%s)' %
-            (inspect.stack()[0][3], e)
+    stdout_message(
+            message='%s: Error while reading or writing post-commit-hook' % inspect.stack()[0][3],
+            prefix='WARN'
         )
 sys.exit(0)
